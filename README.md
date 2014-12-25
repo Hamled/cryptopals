16ebbfa0b61bab0ff03f6a05fdfa8a7a53a75490
Charles Ellis <hamled@hamled.name>
2014-12-24 17:20:42 -0800

    Initial project commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# cryptopals
+
+My attempts at solving the Matasano crypto challenges using Clojure. See http://cryptopals.com/ for details.
+
+## License
+
+Copyright © 2014 Charles Ellis
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cryptopals "0.1.0-SNAPSHOT"
+  :description "My attempts at the Matasano crypto challenges"
+  :url "http://cryptopals.com/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [byte-streams "0.2.0-alpha3"]])
