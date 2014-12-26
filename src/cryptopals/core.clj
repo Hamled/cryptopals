806cf365b4ffcf4464fdd0becc0ca38a2762deae
Charles Ellis <hamled@hamled.name>
2014-12-26 09:25:00 -0800

    Function for conversion of hex to byte array

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -6,3 +6,10 @@
   (if (> (count hex) 2)
     (throw (IllegalArgumentException. "More than two hexadecimal characters cannot be converted to a byte."))
     (Integer/parseInt (apply str hex) 16)))
+
+(defn hex-to-byte-array
+  "Convert a string of hex into a byte array."
+  [hex]
+  (if (odd? (count hex))
+    (hex-to-byte-array (str \0 (apply str hex)))
+    (byte-array (map hex-to-byte (partition 2 hex)))))
