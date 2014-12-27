d0e9464f1d02f11178f3020cf43819959d796096
Charles Ellis <hamled@hamled.name>
2014-12-27 12:08:37 -0800

    Use org.apache.commons.codec for hex/base64 tasks

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [byte-streams "0.2.0-alpha3"]])
+                 [commons-codec "1.10"]])
--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,15 +1,1 @@
 (ns cryptopals.core)
-
-(defn hex-to-byte
-  "Convert up to two hex characters into a byte."
-  [hex]
-  (if (> (count hex) 2)
-    (throw (IllegalArgumentException. "More than two hexadecimal characters cannot be converted to a byte."))
-    (Integer/parseInt (apply str hex) 16)))
-
-(defn hex-to-byte-array
-  "Convert a string of hex into a byte array."
-  [hex]
-  (if (odd? (count hex))
-    (hex-to-byte-array (str \0 (apply str hex)))
-    (byte-array (map hex-to-byte (partition 2 hex)))))
