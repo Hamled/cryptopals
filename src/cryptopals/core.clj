fe3531677c7e75a73da9937a21d2e251faa09b3a
Charles Ellis <hamled@hamled.name>
2014-12-27 12:57:47 -0800

    Clojure wrappers for commons codec hex functions

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,1 +1,12 @@
-(ns cryptopals.core)
+(ns cryptopals.core
+  (:import (org.apache.commons.codec.binary Hex)))
+
+(defn decode-hex
+  "Decode a string of hexadecimal values into a byte array."
+  [hex]
+  (Hex/decodeHex (char-array hex)))
+
+(defn encode-hex
+  "Encode a byte array into a string of hexadecimal values."
+  [byte-values]
+  (apply str (Hex/encodeHex byte-values true)))
--- test/cryptopals/core_test.clj
+++ test/cryptopals/core_test.clj
@@ -1,3 +1,13 @@
 (ns cryptopals.core-test
   (:require [clojure.test :refer :all]
-            [cryptopals.core :refer :all]))
+            [cryptopals.core :refer :all])
+  (:import [java.util Arrays]))
+
+(def test-hex "0123456789abcdefdeadbeefdeadbeefdeadbeefdeadbeef")
+(def test-bytes (byte-array [0 1 78 127 -1 -127 -78 37 19 64 -64]))
+
+(deftest hex-conversion
+  (testing "decoding -> encoding round trip"
+    (= test-hex (encode-hex (decode-hex test-hex))))
+  (testing "encoding -> decoding round trip"
+    (Arrays/equals test-bytes (decode-hex (encode-hex test-bytes)))))
