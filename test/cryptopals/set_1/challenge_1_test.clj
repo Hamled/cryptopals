1a93c04bf2a192109485f2fb490edb4b685ae53c
Charles Ellis <hamled@hamled.name>
2014-12-27 15:51:19 -0800

    Fix test defintions
    I was not using (is ...) and this was causing all tests to appear to
    pass, even though their conditions were not actually being evaluated.

--- test/cryptopals/set_1/challenge_1_test.clj
+++ test/cryptopals/set_1/challenge_1_test.clj
@@ -8,6 +8,6 @@
               49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
             should produce:
               SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-       (Base64/encodeBase64String (decode-hex
-              "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))))
+    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+           (Base64/encodeBase64String (decode-hex
+              "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))))))
--- test/cryptopals/set_1/challenge_3_test.clj
+++ test/cryptopals/set_1/challenge_3_test.clj
@@ -8,5 +8,5 @@
   (testing "The hex encoded string:
               1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
            ... has been XOR'd against a single character. Find the key, decrypt the message."
-    (= "Cooking MC's like a pound of bacon"
-       (ffirst (find-plaintext ciphertext-hex)))))
+    (is (= ["Cooking MC's like a pound of bacon"]
+           (ffirst (find-plaintext ciphertext-hex))))))
