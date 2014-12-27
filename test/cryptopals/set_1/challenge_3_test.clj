6484d7d43971121e6a7732cd01f9f9c446e1c06d
Charles Ellis <hamled@hamled.name>
2014-12-27 15:52:38 -0800

    Fix challenge 3 test
    Should have been comparing nfirst instead of ffirst.

--- test/cryptopals/set_1/challenge_3_test.clj
+++ test/cryptopals/set_1/challenge_3_test.clj
@@ -9,4 +9,4 @@
               1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
            ... has been XOR'd against a single character. Find the key, decrypt the message."
     (is (= ["Cooking MC's like a pound of bacon"]
-           (ffirst (find-plaintext ciphertext-hex))))))
+           (nfirst (find-plaintext ciphertext-hex))))))
