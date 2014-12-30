6a5c49a1ed2f7280ddc115b66e9413e4c1023b0b
Charles Ellis <hamled@hamled.name>
2014-12-29 18:12:45 -0800

    Use combinatorics library for cartesian product

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [commons-codec "1.10"]
-                 [clj-fuzzy  "0.1.8"]])
+                 [clj-fuzzy  "0.1.8"]
+                 [org.clojure/math.combinatorics "0.0.8"]])
